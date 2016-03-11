afb9c67114f3f33e3e6fe76808a6c2e354bb7774
Pavani ios <pavani_ios@asmans-mac-mini-2.local>
2016-03-11 11:44:01 +0530

    Initial Commit

---
+++ TextColors/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextColors
+//
+//  Created by Pavani_ios on 3/11/16.
+//  Copyright © 2016 Pavani_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TextColors/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TextColors
+//
+//  Created by Pavani_ios on 3/11/16.
+//  Copyright © 2016 Pavani_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
